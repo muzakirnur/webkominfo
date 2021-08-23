9957dc368c032e35f84a97af8c91784ba6ed207a
Muzakir Nur <89138105+muzakirnur@users.noreply.github.com>
2021-08-23 21:45:49 +0700

    Membetulkan relasi tabel

---
+++ database/migrations/2021_08_23_144015_create_permohonans_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermohonansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permohonans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('no_tiket');
+            $table->string('topik');
+            $table->string('judul');
+            $table->string('deskripsi');
+            $table->string('lampiran');
+            $table->bigInteger('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
+            $table->string('progres');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permohonans');
+    }
+}
