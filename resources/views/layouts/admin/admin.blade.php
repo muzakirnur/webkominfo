f4f6dc1369a585d4f42508bc9b2490812b6f0f03
Muzakir Nur <89138105+muzakirnur@users.noreply.github.com>
2021-08-24 22:58:44 +0700

    Menambahkan Favicon

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -11,6 +11,17 @@
 
     <title>Web Kominfo - {{ $page }}</title>
 
+    {{-- Favicon Website --}}
+    <link rel="shortcut icon" href="https://kominfo.go.id/favicon/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="https://kominfo.go.id/favicon/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="https://kominfo.go.id/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="https://kominfo.go.id/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="https://kominfo.go.id/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="https://kominfo.go.id/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="https://kominfo.go.id/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="https://kominfo.go.id/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="https://kominfo.go.id/favicon/apple-touch-icon-152x152.png" />
+
     <!-- Custom fonts for this template-->
     <link href="{{ asset('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
